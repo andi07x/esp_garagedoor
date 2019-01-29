fe2d0202001377f23cdc9543ec25ca6721f2a0bc
andi07x <andi07x@users.noreply.github.com>
2019-01-29 22:31:34 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+const Service, Characteristic;
+ 
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("esp-garagedoor", "MyGaragedoorOpener", myGdOpener);
+};
+
+function mySwitch(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+
+myGdOpener.prototype = {
+  getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "My GdOpener manufacturer")
+      .setCharacteristic(Characteristic.Model, "My GdOpener model")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+ 
+    let switchService = new Service.Switch("My GdOpener");
+    switchService
+      .getCharacteristic(Characteristic.On)
+        .on('get', this.getSwitchOnCharacteristic.bind(this))
+        .on('set', this.setSwitchOnCharacteristic.bind(this));
+ 
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+  }
+
+getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+        url: me.getUrl,
+        method: 'GET',
+    }, 
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.currentState);
+    });
+  },
+   
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
